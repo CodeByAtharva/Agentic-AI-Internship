7f0823d1a05ba284d5bb747522eda0fb5e98884e
Atharva <athrudev@gmail.com>
2025-12-29 19:37:39 +0530

    Completed the day 2 of internship

--- day 2/main.py
+++ day 2/main.py
@@ -1,5 +1,8 @@
 import csv
-with open("people1.csv","r") as f:
+from app import add
+'''with open("people1.csv","r") as f:
     r=csv.reader(f)
     for i in r:
-        print(i)
+        print(i)'''
+
+print(add(2,3))
